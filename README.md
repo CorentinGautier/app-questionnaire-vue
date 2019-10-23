9b9306f7dec4ad33e6c6b3a6bb25d417634bd23b
CorentinGautier <43545800+CorentinGautier@users.noreply.github.com>
2019-10-23 12:02:19 +0200

    debut redirection

--- README.md
+++ README.md
@@ -27,3 +27,12 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Je suis perdu 
+### Je suis perdu 
+
+### Je suis perdu 
+### Je suis perdu 
+
+### Je suis perdu 
+
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,17 @@
 <template>
   <div id='login'>
    <h1>Login</h1>
+   <form submit prevent="false">
+    <label for="cguserLastname">LastName</label>
     <input type='text' name='userLastname' v-model='input.cguserLastname' placeholder='Nom' />
+
+    <label for="cguserFirstname">FirstName</label>
     <input type='text' name='userFirstname' v-model='input.cguserFirstname' placeholder='Prénom' />
-    <input
-      type='text'
-      name='societyName'
-      v-model='input.cgsocietyName'
-      placeholder='Nom de la société'
-    />
-    <button type='button' v-on:click='login()'>Login</button>
+
+    <label for="cgsocietyName">Society Name</label>
+    <input type='text' name='societyName' v-model='input.cgsocietyName' placeholder='Nom de la société'/>
+    <button type='submit' v-on:click.stop='login()'>Begin the test</button>
+    </form>
   </div>
 </template>
 <script>
@@ -27,10 +29,9 @@ export default {
   methods: {
     login () {
       if (this.cguserFirstname !== '' && this.cguserLastname !== '' && this.cgsocietyName !== '') {
-        return true
+        this.$routeur.push('/questionnaire?cguserFirstname=' + this.cguserFirstname + '&cguserLastName' + this.cguserLastName + '$cgsocietyName=' + this.cgsocietyName)
       } else {
         console.log = 'Please put your first.last name and your society name.'
-        return false
       }
     }
   }
